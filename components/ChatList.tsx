921c9d9587d8ff87e290a650e9cd11517440791f
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2023-04-22 21:19:12 +0100

    show ens in chatList.tsx

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -10,9 +10,10 @@ import { useEnsAddress } from "wagmi";
 import { ChatMode, FetchStatus } from "@/lib/types";
 import Copy from "./Copy";
 
-interface ChatListProps{
-  address: string,
-  mode: ChatMode
+interface ChatListProps {
+  address: string;
+  mode: ChatMode;
+  ensNameForAddress?: string | null;
 }
 
 const copyMessage = {
@@ -26,12 +27,16 @@ const copyMessage = {
   ],
 };
 
-function ChatList(props: ChatListProps){
-  const {address, mode} = props;
+function ChatList(props: ChatListProps) {
+  const { address, mode, ensNameForAddress } = props;
   const [bounce, setBounce] = useState("");
   const [filterFor, _setFilterFor] = useState("");
 
-  const {discussions: _discussions, fetchStatus, loaded} = useDiscussions(address)
+  const {
+    discussions: _discussions,
+    fetchStatus,
+    loaded,
+  } = useDiscussions(address);
 
   //sort in descending order of timestamp
   const discussions = Object.entries(_discussions || []).sort(
@@ -52,7 +57,6 @@ function ChatList(props: ChatListProps){
     if (data) _setFilterFor(data.toLowerCase());
   }, [filterFor]);
 
-
   const setFilterFor = useCallback(
     (e: ChangeEvent<HTMLInputElement>) =>
       _setFilterFor(
@@ -67,7 +71,6 @@ function ChatList(props: ChatListProps){
     address && discussions.length === 0 ? startBounce() : setBounce("");
   }, [address, discussions]);
 
-
   return (
     <div className="border shadow-2xl py-8 flex flex-col rounded-3xl h-full w-full sm:w-4/6 lg:w-3/6 xl:w-2/6">
       {/**top section with create new message icon */}
@@ -76,7 +79,9 @@ function ChatList(props: ChatListProps){
           <div className="text-xs xs:text-base sm:text-xl font-bold">
             {mode === ChatMode.CHAT
               ? "Your Messages"
-              : `Messages for ${shorten(address ?? "")}`}
+              : !ensNameForAddress
+              ? `Messages for ${shorten(address ?? "")}`
+              : `Messages for ${ensNameForAddress}`}
           </div>
           <div className="hidden sm:block">
             <Copy
@@ -91,9 +96,7 @@ function ChatList(props: ChatListProps){
               }/${mode}/${address?.toLowerCase()}`}
             />
           </div>
-          <div
-            className="block sm:hidden"
-          >
+          <div className="block sm:hidden">
             <Copy
               onCopyText="Copied!"
               defaultText={
