7b6292125e5f90fcb29ae0088bbc0a44665d3fc7
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2023-04-25 18:42:55 +0100

    add ens names to show top of preview list

--- pages/watch/[address].tsx
+++ pages/watch/[address].tsx
@@ -6,12 +6,18 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { isAddress } from "ethers/lib/utils.js";
 import { ChatMode } from "@/lib/types";
+import { useEnsName } from "wagmi";
 
 ReactGA.initialize("UA-262892775-1");
 
 const Home: NextPage = () => {
   const router = useRouter()
   const {address} = router.query;
+  const { data } = useEnsName({
+    address: `0x${address?.slice(2) as string}`,
+    chainId: 1,
+  });
+
   useEffect(() => {
     if (address && (typeof address !== "string" || !isAddress(address)) && router) {
       router.push("/");
@@ -21,7 +27,7 @@ const Home: NextPage = () => {
   return (
     <AppLayout>
       {typeof address === "string" && isAddress(address) && (
-        <ChatList mode={ChatMode.WATCH} address={address as string} />
+        <ChatList ensNameForAddress={data} mode={ChatMode.WATCH} address={address as string} />
       )}
     </AppLayout>
   );
